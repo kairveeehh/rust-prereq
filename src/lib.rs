41b97cd62bb8e1966725736e283db7698afe1c49
kairvee <imt_2023037@iiitm.ac.in>
2025-01-09 07:10:10 +0530

    completed transfer of 0.001 sol task

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 #[cfg(test)]
 mod tests {
-    use solana_sdk::signature::{Keypair, Signer};
     use bs58;
-    use solana_client::rpc_client::RpcClient; 
-    use solana_sdk::signer::keypair::read_keypair_file;
-    use std::io;
-    use std::io::BufRead;
-  
+    use solana_client::{nonblocking::rpc_client, rpc_client::RpcClient};
+    use solana_program::{pubkey::Pubkey, system_instruction::transfer};
+    use solana_sdk::{
+        signature::{self, read_keypair_file, Keypair, Signer},
+        signer::keypair,
+        transaction::{self, Transaction},
+    };
+    use std::io::{self, stdin, BufRead};
+    use std::str::FromStr;
     const RPC_URL: &str = "https://api.devnet.solana.com";
 
     #[test]
@@ -47,6 +50,28 @@ mod tests {
             Err(e) => println!("Oops, something went wrong: {}", e.to_string()),
         };
       // txid -   https://explorer.solana.com/tx/3AfnQLa3JwYWNwHqMatjwRFHouY8ty34zhSuhradb1Aa3AtDzdQbQcimS5wHSQXxVgtUE5bpnaDvbSs8PUVrfVHz?cluster=devnet
+    }
+
+    #[test]
+    fn transfer_sol() {
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        let to_pubkey = Pubkey::from_str("86gsWCEgo8VRr4rA3Bznh3jAArhL4TqrTBifxfyQfgzP").unwrap();
+        let rpc_client = RpcClient::new(RPC_URL);
+        let recent_blockhash = rpc_client.get_latest_blockhash().expect("Failed to get ");
+        let transaction = Transaction::new_signed_with_payer(
+            &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
+            Some(&keypair.pubkey()),
+            &vec![&keypair],
+            recent_blockhash,
+        );
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Check out your TX here: https://explorer.solana.com/tx/{}?cluster=devnet",
+            signature
+        );
+        //Transfer 0.001 SOL -  https://explorer.solana.com/tx/2Np3xkm5UdQHtjQbiJiFGoWDU1SYV1U6UxZQFBDoFrv7jCmnX8HZvrz3UAPHG4KPywYvaPA4ohVfBc41WtJceaof?cluster=devnet
     }
 
 }
