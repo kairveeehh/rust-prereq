4330f1416db988136353c411d400ae3954056c1b
kairvee <imt_2023037@iiitm.ac.in>
2025-01-09 07:33:59 +0530

    completed enrollment task

---
+++ README.md
@@ -0,0 +1,11 @@
+## Task 1 
+dev wallet - CkutiP1SHQKB1EaKt9zyaNxSQEHfKss9rAwpB6MUyGk3
+## Task 2 
+ Txid id -  https://explorer.solana.com/tx/3AfnQLa3JwYWNwHqMatjwRFHouY8ty34zhSuhradb1Aa3AtDzdQbQcimS5wHSQXxVgtUE5bpnaDvbSs8PUVrfVHz?cluster=devnet
+## Task 3
+transfer 0.001 SOL -  https://explorer.solana.com/tx/2Np3xkm5UdQHtjQbiJiFGoWDU1SYV1U6UxZQFBDoFrv7jCmnX8HZvrz3UAPHG4KPywYvaPA4ohVfBc41WtJceaof?cluster=devnet
+
+complete wallet drain txid - https://explorer.solana.com/tx/4DnQi6hUfUxpGBEi3RfpF48RExhQbKRtrT9CWvrKce5rrtVBSW1BsJiCxLzD3XVSaDHWBDHR1j8b2qnGe5BQjQPG?cluster=devnet
+
+## Task 4 
+Txid - https://explorer.solana.com/tx/5wMk9CiheUJVCnF2UZa8szSuCfeHusiZ4FcRfeVLk5zbGg9rxz98kFF4h5yRjtqhBVmbLNH2j1RgnRCYTK14c6KY?cluster=devnet
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
+mod programs;
+use crate::programs::Turbin3_prereq::{CompleteArgs, Turbin3PrereqProgram, UpdateArgs};
+
+
+
 #[cfg(test)]
+
 mod tests {
     use bs58;
     use solana_client::{nonblocking::rpc_client, rpc_client::RpcClient};
     use solana_program::{pubkey::Pubkey, system_instruction::transfer};
     use solana_sdk::{
         message::Message,
-        signature::{read_keypair_file, Keypair, Signer},
-        signer::keypair,
+        signature::{self, read_keypair_file, Keypair, Signer},
+        signer::{self, keypair},
+        system_program,
         transaction::Transaction,
     };
     use std::io::{self, stdin, BufRead};
     use std::str::FromStr;
+    
+    use crate::programs::Turbin3_prereq::{CompleteArgs, Turbin3PrereqProgram};
     const RPC_URL: &str = "https://api.devnet.solana.com";
 
     #[test]
@@ -86,6 +95,37 @@ mod tests {
         //Transfer 0.001 SOL -  https://explorer.solana.com/tx/2Np3xkm5UdQHtjQbiJiFGoWDU1SYV1U6UxZQFBDoFrv7jCmnX8HZvrz3UAPHG4KPywYvaPA4ohVfBc41WtJceaof?cluster=devnet
 
         // complete wallet drain txid - https://explorer.solana.com/tx/4DnQi6hUfUxpGBEi3RfpF48RExhQbKRtrT9CWvrKce5rrtVBSW1BsJiCxLzD3XVSaDHWBDHR1j8b2qnGe5BQjQPG?cluster=devnet
+    }
+
+    #[test]
+    fn enroll() {
+        let rpc_client = RpcClient::new(RPC_URL);
+        let signer = read_keypair_file("turbine.json").expect("Couldn't find wallet file");
+        let prereq = Turbin3PrereqProgram::derive_program_address(&[
+            b"prereq",
+            signer.pubkey().to_bytes().as_ref(),
+        ]);
+        let args = CompleteArgs {
+            github: b"kairveeehh".to_vec(),
+        };
+        let blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+        let transaction = Turbin3PrereqProgram::complete(
+            &[&signer.pubkey(), &prereq, &system_program::id()],
+            &args,
+            Some(&signer.pubkey()),
+            &[&signer],
+            blockhash,
+        );
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Checkout your TX here: https://explorer.solana.com/tx/{}?cluster=devnet",
+            signature
+        );
+        //Txid -  https://explorer.solana.com/tx/5wMk9CiheUJVCnF2UZa8szSuCfeHusiZ4FcRfeVLk5zbGg9rxz98kFF4h5yRjtqhBVmbLNH2j1RgnRCYTK14c6KY?cluster=devnet
     }
 
 }
