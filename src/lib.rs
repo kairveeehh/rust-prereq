894332dffd5312c66d1a7950ef015cd76a9d99d2
kairvee <imt_2023037@iiitm.ac.in>
2025-01-09 07:13:43 +0530

    completed complete transfer task

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ mod tests {
     use solana_client::{nonblocking::rpc_client, rpc_client::RpcClient};
     use solana_program::{pubkey::Pubkey, system_instruction::transfer};
     use solana_sdk::{
-        signature::{self, read_keypair_file, Keypair, Signer},
+        message::Message,
+        signature::{read_keypair_file, Keypair, Signer},
         signer::keypair,
-        transaction::{self, Transaction},
+        transaction::Transaction,
     };
     use std::io::{self, stdin, BufRead};
     use std::str::FromStr;
@@ -57,9 +58,20 @@ mod tests {
         let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
         let to_pubkey = Pubkey::from_str("86gsWCEgo8VRr4rA3Bznh3jAArhL4TqrTBifxfyQfgzP").unwrap();
         let rpc_client = RpcClient::new(RPC_URL);
+        let balance = rpc_client
+        .get_balance(&keypair.pubkey())
+        .expect("Failed to get balance");
         let recent_blockhash = rpc_client.get_latest_blockhash().expect("Failed to get ");
+        let message = Message::new_with_blockhash(
+            &[transfer(&keypair.pubkey(), &to_pubkey, balance)],
+            Some(&keypair.pubkey()),
+            &recent_blockhash,
+        );
+        let fee = rpc_client
+            .get_fee_for_message(&message)
+            .expect("Failed to get fee calculator");
         let transaction = Transaction::new_signed_with_payer(
-            &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
+            &[transfer(&keypair.pubkey(), &to_pubkey, balance-fee)],
             Some(&keypair.pubkey()),
             &vec![&keypair],
             recent_blockhash,
@@ -72,6 +84,8 @@ mod tests {
             signature
         );
         //Transfer 0.001 SOL -  https://explorer.solana.com/tx/2Np3xkm5UdQHtjQbiJiFGoWDU1SYV1U6UxZQFBDoFrv7jCmnX8HZvrz3UAPHG4KPywYvaPA4ohVfBc41WtJceaof?cluster=devnet
+
+        // complete wallet drain txid - https://explorer.solana.com/tx/4DnQi6hUfUxpGBEi3RfpF48RExhQbKRtrT9CWvrKce5rrtVBSW1BsJiCxLzD3XVSaDHWBDHR1j8b2qnGe5BQjQPG?cluster=devnet
     }
 
 }
